89d8906156068a7bf3f2ff4c1ac5cbddece446ea
johnpasichana <john.pasichana@correounivalle.edu.co>
2024-04-26 11:21:47 -0500

    small test 3

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,11 +5,11 @@ var cors = require("cors");
 const { MongoClient, ObjectId, ServerApiVersion  } = require('mongodb');
 
 var app = express();
-app.use(cors());
-// app.use(cors(
-//   {
-//     origin: process.env.CLIENT_URL,
-//   }
+
+app.use(cors(
+  {
+    origin: process.env.CLIENT_URL,
+  }
 // ))
 
 // Middleware
