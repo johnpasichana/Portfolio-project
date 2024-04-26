6ab4afddf1607db8fa51f8d1aac551a2a6651f7c
johnpasichana <john.pasichana@correounivalle.edu.co>
2024-04-26 05:24:28 -0500

    npm start added

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,9 +8,7 @@ export default defineConfig({
            index: path.resolve(__dirname, 'src/index.html'),
            comments: path.resolve(__dirname, 'src/pages/comments/comments.html'),
            contact: path.resolve(__dirname, 'src/pages/contact/contact.html'),
-           guide: path.resolve(__dirname, 'src/pages/guide/guide.html'),
-           play: path.resolve(__dirname, 'src/pages/play/play.html'),
-           videos: path.resolve(__dirname, 'src/pages/videos/videos.html'),
+           play: path.resolve(__dirname, 'src/pages/projects/projects.html'),
         }
     },
     outDir: 'dist',
